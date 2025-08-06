15c995ea8ef7a10c5f0fc9a237db14e940cfbb17
Wake1101 <projektwake03@gmail.com>
2025-08-06 20:47:48 -0300

    Creating new expense object

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 // Capturando os inputs do formulário
-const amount = document.getElementById("amount");
+const form = document.querySelector("form"); // formulario
+const amount = document.getElementById("amount"); // valor da despesa
+const exepense = document.getElementById("expense"); // nome da despesa
+const category = document.getElementById("category"); // categorai da despesa
 
 // Formatando o valor do input
 amount.oninput = () => {
@@ -12,11 +15,29 @@ amount.oninput = () => {
    amount.value = formatCurrencyBRL(value); // Aplicando a formatação
 }
 
+// Formatando o valor para a moeda brasileira
 function formatCurrencyBRL(value) {
    value = value.toLocaleString("pt-BR", {
       style: "currency",
       currency: "BRL",
    });
 
-   return value
+   // Retornando o valor
+   return value;
+}
+
+// Adicionando os eventos do formulário
+form.onsubmit = (event) => {
+   // evitando o reset da página durente o submit
+   event.preventDefault();
+
+   // criando um obejto da nova despesa
+   const newExpense = {
+      id: new Date().getTime(), // id da despesa
+      expense: expense.value, // nome
+      category_id: category.value, // id do tipo da despesa
+      category_name: category.options[category.selectedIndex].text, // nome do tipo da despesa
+      amount: amount.value, // valor da despesa
+      created_at: new Date(), // quando foi criada a despesa
+   }
 }
