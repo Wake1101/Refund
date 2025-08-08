843c1d5035364a251ca7fe6cec1e90c11326b64c
Wake1101 <projektwake03@gmail.com>
2025-08-07 21:13:15 -0300

    Adding list item and icon

--- scripts.js
+++ scripts.js
@@ -2,7 +2,10 @@
 const form = document.querySelector("form"); // formulario
 const amount = document.getElementById("amount"); // valor da despesa
 const exepense = document.getElementById("expense"); // nome da despesa
-const category = document.getElementById("category"); // categorai da despesa
+const category = document.getElementById("category"); // categoria da despesa
+
+// Elementos da lista
+const expenseList = document.querySelector("ul"); // Lista de despesas
 
 // Formatando o valor do input
 amount.oninput = () => {
@@ -39,5 +42,31 @@ form.onsubmit = (event) => {
       category_name: category.options[category.selectedIndex].text, // nome do tipo da despesa
       amount: amount.value, // valor da despesa
       created_at: new Date(), // quando foi criada a despesa
+   }
+
+   // chamando a função que cria o item na lista
+   expenseAdd(newExpense);
+}
+
+function expenseAdd(newExpense) {
+   try {
+      // criando os elementos para colocar na lista
+      const expenseItem = document.createElement("li"); // item da lista
+      expenseItem.classList.add("expense"); // adicionando a classe do item
+
+      // Criando o icone
+      const expenseIcon = document.createElement("img"); // Criando a imagem
+      expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`); // pegando o icone da categoria
+      expenseIcon.setAttribute("alt", expenseItem.category_name); // adicionando descriçao do icone
+
+      // Adicionando os dados ao item da lista
+      expenseItem.append(expenseIcon);
+
+      // Adicionando o item a lista
+      expenseList.append(expenseItem);
+
+   } catch (error) {
+      alert("Não foi possível adicionar o item na lista"); // exibindo uma mensagem para o usuario
+      console.log(error); // exibindo o erro no console
    }
 }
